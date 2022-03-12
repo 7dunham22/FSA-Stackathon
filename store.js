5c75e6e609da1f1b605ddc21bdae1ae8df671f6e
David Dunham <dd567@cornell.edu>
2022-03-11 22:11:13 -0500

    converted store.js to the format of the model

--- store.js
+++ store.js
@@ -1,21 +1,59 @@
+import { useMemo } from 'react';
 import { createStore, applyMiddleware } from 'redux';
-import appReducer from './redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import reducers from './redux';
 
-const RESET_STORE = 'RESET_STORE';
-export const resetStore = () => ({ type: RESET_STORE });
-const rootReducer = (state = {}, action) => {
-  if (action.type === RESET_STORE) {
-    state = undefined;
-    return appReducer(state, action);
+let store;
+
+function initStore(initialState) {
+  return createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  );
+}
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState);
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
   }
-  return appReducer(state, action);
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store;
+  // Create the store once in the client
+  if (!store) store = _store;
+
+  return _store;
 };
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware, createLogger())
-);
+export function useStore(initialState) {
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  return store;
+}
 
-export default store;
+// const RESET_STORE = 'RESET_STORE';
+// export const resetStore = () => ({ type: RESET_STORE });
+// const rootReducer = (state = {}, action) => {
+//   if (action.type === RESET_STORE) {
+//     state = undefined;
+//     return appReducer(state, action);
+//   }
+//   return appReducer(state, action);
+// };
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunkMiddleware, createLogger())
+// );
+
+// export default store;
