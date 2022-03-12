6644a821ddb6e160c98ce96f5a41333c10cf846b
David Dunham <dd567@cornell.edu>
2022-03-11 22:20:25 -0500

    filled out initial _app file

--- store.js
+++ store.js
@@ -15,7 +15,8 @@ function initStore(initialState) {
 }
 
 export const initializeStore = (preloadedState) => {
-  let _store = store ?? initStore(preloadedState);
+  // let _store = store ?? initStore(preloadedState);
+  let _store = store ? store : initStore(preloadedState);
 
   // After navigating to a page with an initial Redux state, merge that state
   // with the current state in the store, and create a new store
@@ -40,6 +41,8 @@ export function useStore(initialState) {
   const store = useMemo(() => initializeStore(initialState), [initialState]);
   return store;
 }
+
+/* THE STORE SCRIPT FROM THE VERSION BEFORE IMPLEMENTING NEXT.JS */
 
 // const RESET_STORE = 'RESET_STORE';
 // export const resetStore = () => ({ type: RESET_STORE });
