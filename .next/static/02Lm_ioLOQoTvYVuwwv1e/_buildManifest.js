2419811a17444f0f84e29a7597021d2b0610f517
David Dunham <dd567@cornell.edu>
2022-03-12 20:58:57 -0500

    Further installations. See package.json

---
+++ .next/static/02Lm_ioLOQoTvYVuwwv1e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-eab3ecf9e08ce513.js"],"/_error":["static/chunks/pages/_error-cf63cf6e3610bbd6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
